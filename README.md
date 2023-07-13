75555644cf6000d431751712bab4ceb1cad7aadb
ZacharyGo-TutorialsPractice <zacharyseego@gmail.com>
2023-07-13 22:22:06 +0800

    1. Understanding JPA6. Challenge - Create simple employee, company, and salary classes

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 "# JavaPersistenceWithJPA" 
+1. Understanding JPA	6. Challenge - Create simple employee, company, and salary classes
---
+++ src/main/java/com/linkedinlearning/jpa/Main.java
@@ -0,0 +1,22 @@
+package com.linkedinlearning.jpa;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+import jakarta.persistence.PersistenceContext;
+
+public class Main {
+
+    @PersistenceContext
+    EntityManager entityManager;
+
+    public static void main(String[] args) {
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("default");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+        System.out.println("Don't forget to launch Postgres before running this code!");
+
+        entityManager.close();
+        entityManagerFactory.close();
+    }
+}
